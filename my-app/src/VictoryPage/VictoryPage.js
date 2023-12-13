a1a6496a3d2a727d1bf4c8ac24c548b22e0fd211
Fred1178 <54545786+Fred1178@users.noreply.github.com>
2023-12-12 18:55:30 -0600

    updated VictoryPage

--- my-app/src/VictoryPage/VictoryPage.js
+++ my-app/src/VictoryPage/VictoryPage.js
@@ -1,35 +1,35 @@
 import './VictoryPage.css';
+import { useState } from 'react';
 
-//need to figure out how to display enemy loot
+
 function VictoryPage({onReturnClick, enemy, player}) {
-    /*
-    const victoryText = [ ];
-    function addText(victoryText, onReturnClick, enemy, player) {
-        const text = victoryText;
-        text.push(`You defeated the ${enemy.name}!`);
-        text.push(`You got: `);
-        text.push(`${enemy.loot.potions} potion(s)`);
-        return text;
-    }
-*/
-   
 
-    //<addText victoryText={victoryText} onReturnClick={onReturnClick} enemy={enemy} player={player} />
+    const [showLevelUp, setShowLevelUp] = useState(false);
 
     return (
         <div className="victoryScreen">
-            <p>You won!</p>
-            <p>You got: </p>
-            <p>{enemy.loot.potions} potion(s)</p>
-            <p>{enemy.loot.gold} gold</p>
+            <div>
+                <p>You defeated the {enemy.name}!</p>
+                <p>You got: </p>
+                <p>{enemy.loot.potions} potion(s)</p>
+                <p>{enemy.loot.gold} gold</p>
+                <button type="button" 
+                    onClick={() => setShowLevelUp(!showLevelUp)}> Continue </button>
+            </div>
             
-            <p>You feel yourself grow stronger!</p>
-            <p>You leveled up to level {player.level}</p>
-            <p>Your damage increased to {player.damage}</p>
-            <p>Your max health increases to {player.maxHealth}</p>
-            <button type="button" onClick={onReturnClick}>Return</button>
+
+            {showLevelUp && 
+                <div>
+                    <p>You feel yourself grow stronger!</p>
+                    <p>You leveled up to level {player.level}</p>
+                    <p>Your damage increased to {player.damage}</p>
+                    <p>Your max health increases to {player.maxHealth}</p>
+                    <button type="button" onClick={onReturnClick}>Return</button>
+                </div>
+            }
         </div>
     );
 }
 
 export default VictoryPage;
+
